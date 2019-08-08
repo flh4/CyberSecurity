66e610cfc93201d355d2cc6e90edb992f91b3827
RickHerzog <51469035+flh4@users.noreply.github.com>
2019-08-08 14:17:21 -0500

    Create rshell.c

---
+++ ReverseShell/rshell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void) {
+
+	int socktt;
+	int port = '4444'
+	
+}
